794a86b3839e9b8d423dba84a2abfc5ee47135ee
phantamanta44 <evandalong@gmail.com>
2020-12-22 12:59:15 -0600

    Fix config not reloading properly when edited via the in-game config GUI

--- src/main/java/xyz/phanta/fluiddrawers/client/ClientProxy.java
+++ src/main/java/xyz/phanta/fluiddrawers/client/ClientProxy.java
@@ -2,7 +2,10 @@ package xyz.phanta.fluiddrawers.client;
 
 import io.github.phantamanta44.libnine.util.nullity.Reflected;
 import net.minecraftforge.client.model.ModelLoaderRegistry;
+import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import xyz.phanta.fluiddrawers.CommonProxy;
+import xyz.phanta.fluiddrawers.client.handler.ConfigGuiHandler;
 import xyz.phanta.fluiddrawers.client.model.FramedTextureModel;
 
 @Reflected
@@ -10,6 +13,12 @@ public class ClientProxy extends CommonProxy {
 
     public ClientProxy() {
         ModelLoaderRegistry.registerLoader(new FramedTextureModel.Loader()); // needs to be registered asap
+    }
+
+    @Override
+    public void onPreInit(FMLPreInitializationEvent event) {
+        super.onPreInit(event);
+        MinecraftForge.EVENT_BUS.register(new ConfigGuiHandler());
     }
 
 }
---
+++ src/main/java/xyz/phanta/fluiddrawers/client/handler/package-info.java
@@ -0,0 +1,4 @@
+@NothingIsNullByDefault
+package xyz.phanta.fluiddrawers.client.handler;
+
+import io.github.phantamanta44.libnine.util.nullity.NothingIsNullByDefault;
